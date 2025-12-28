ebf3380deb1460a9391dbb0ab035920522662bca
Jumblemint <jeepinit77@gmail.com>
2025-12-28 16:11:14 -0600

    New Editor Screen

--- src/editor.ts
+++ src/editor.ts
@@ -1,152 +1,69 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { LitElement, html, TemplateResult, css, CSSResultGroup } from 'lit';
-import { HomeAssistant, fireEvent, LovelaceCardEditor } from 'custom-card-helpers';
+import { LitElement, html, TemplateResult, CSSResultGroup, css } from 'lit';
+import { property, state, customElement } from 'lit/decorators.js';
+import { HomeAssistant, LovelaceCardEditor } from 'custom-card-helpers';
+import { WeatherCardConfig } from './types';
 
-import { ScopedRegistryHost } from '@lit-labs/scoped-registry-mixin';
-import { BoilerplateCardConfig } from './types';
-import { customElement, property, state } from 'lit/decorators';
-import { formfieldDefinition } from '../elements/formfield';
-import { selectDefinition } from '../elements/select';
-import { switchDefinition } from '../elements/switch';
-import { textfieldDefinition } from '../elements/textfield';
-
-@customElement('boilerplate-card-editor')
-export class BoilerplateCardEditor extends ScopedRegistryHost(LitElement) implements LovelaceCardEditor {
+@customElement('nexus-weather-card-editor')
+export class NexusWeatherCardEditor extends LitElement implements LovelaceCardEditor {
   @property({ attribute: false }) public hass?: HomeAssistant;
+  @state() private _config?: WeatherCardConfig;
 
-  @state() private _config?: BoilerplateCardConfig;
-
-  @state() private _helpers?: any;
-
-  private _initialized = false;
-
-  static elementDefinitions = {
-    ...textfieldDefinition,
-    ...selectDefinition,
-    ...switchDefinition,
-    ...formfieldDefinition,
-  };
-
-  public setConfig(config: BoilerplateCardConfig): void {
+  public setConfig(config: WeatherCardConfig): void {
     this._config = config;
-
-    this.loadCardHelpers();
   }
 
-  protected shouldUpdate(): boolean {
-    if (!this._initialized) {
-      this._initialize();
-    }
-
-    return true;
-  }
-
-  get _name(): string {
-    return this._config?.name || '';
-  }
-
-  get _entity(): string {
-    return this._config?.entity || '';
-  }
-
-  get _show_warning(): boolean {
-    return this._config?.show_warning || false;
-  }
-
-  get _show_error(): boolean {
-    return this._config?.show_error || false;
-  }
-
-  protected render(): TemplateResult | void {
-    if (!this.hass || !this._helpers) {
-      return html``;
-    }
-
-    // You can restrict on domain type
-    const entities = Object.keys(this.hass.states);
+  protected render(): TemplateResult {
+    if (!this.hass || !this._config) return html``;
 
     return html`
-      <mwc-select
-        naturalMenuWidth
-        fixedMenuPosition
-        label="Entity (Required)"
-        .configValue=${'entity'}
-        .value=${this._entity}
-        @selected=${this._valueChanged}
-        @closed=${(ev) => ev.stopPropagation()}
-      >
-        ${entities.map((entity) => {
-          return html`<mwc-list-item .value=${entity}>${entity}</mwc-list-item>`;
-        })}
-      </mwc-select>
-      <mwc-textfield
-        label="Name (Optional)"
-        .value=${this._name}
-        .configValue=${'name'}
-        @input=${this._valueChanged}
-      ></mwc-textfield>
-      <mwc-formfield .label=${`Toggle warning ${this._show_warning ? 'off' : 'on'}`}>
-        <mwc-switch
-          .checked=${this._show_warning !== false}
-          .configValue=${'show_warning'}
-          @change=${this._valueChanged}
-        ></mwc-switch>
-      </mwc-formfield>
-      <mwc-formfield .label=${`Toggle error ${this._show_error ? 'off' : 'on'}`}>
-        <mwc-switch
-          .checked=${this._show_error !== false}
-          .configValue=${'show_error'}
-          @change=${this._valueChanged}
-        ></mwc-switch>
-      </mwc-formfield>
+      <div class="card-config">
+        <ha-entity-picker
+          .label="${this.hass.localize('ui.panel.lovelace.editor.card.generic.entity')} (Required)"
+          .hass=${this.hass}
+          .value=${this._config.entity}
+          .configValue=${'entity'}
+          @value-changed=${this._valueChanged}
+          allow-custom-entity
+        ></ha-entity-picker>
+        
+        <div class="side-by-side">
+          <ha-formfield .label=${"Show Wind Speed"}>
+            <ha-switch
+              .checked=${this._config.show_wind !== false}
+              .configValue=${'show_wind'}
+              @change=${this._valueChanged}
+            ></ha-switch>
+          </ha-formfield>
+
+          <ha-formfield .label=${"Show Rain Probability"}>
+            <ha-switch
+              .checked=${this._config.show_rain_prob !== false}
+              .configValue=${'show_rain_prob'}
+              @change=${this._valueChanged}
+            ></ha-switch>
+          </ha-formfield>
+        </div>
+      </div>
     `;
   }
 
-  private _initialize(): void {
-    if (this.hass === undefined) return;
-    if (this._config === undefined) return;
-    if (this._helpers === undefined) return;
-    this._initialized = true;
-  }
-
-  private async loadCardHelpers(): Promise<void> {
-    this._helpers = await (window as any).loadCardHelpers();
-  }
-
   private _valueChanged(ev): void {
-    if (!this._config || !this.hass) {
-      return;
-    }
+    if (!this._config || !this.hass) return;
     const target = ev.target;
-    if (this[`_${target.configValue}`] === target.value) {
-      return;
-    }
-    if (target.configValue) {
-      if (target.value === '') {
-        const tmpConfig = { ...this._config };
-        delete tmpConfig[target.configValue];
-        this._config = tmpConfig;
-      } else {
-        this._config = {
-          ...this._config,
-          [target.configValue]: target.checked !== undefined ? target.checked : target.value,
-        };
-      }
-    }
-    fireEvent(this, 'config-changed', { config: this._config });
+    const value = target.checked !== undefined ? target.checked : ev.detail.value;
+
+    this._config = { ...this._config, [target.configValue]: value };
+    const event = new CustomEvent('config-changed', {
+      detail: { config: this._config },
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(event);
   }
 
-  static styles: CSSResultGroup = css`
-    mwc-select,
-    mwc-textfield {
-      margin-bottom: 16px;
-      display: block;
-    }
-    mwc-formfield {
-      padding-bottom: 8px;
-    }
-    mwc-switch {
-      --mdc-theme-secondary: var(--switch-checked-color);
-    }
-  `;
+  static get styles(): CSSResultGroup {
+    return css`
+      .side-by-side { display: flex; gap: 20px; margin-top: 15px; }
+    `;
+  }
 }
